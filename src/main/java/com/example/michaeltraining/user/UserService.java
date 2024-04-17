e645dd6010b7f8eaf4dff081a5aad89185329260
Toukach Mikhail <mikhail2.ru@mail.ru>
2024-04-17 07:34:38 +0300

    last update for working_branch adding update_user method

--- src/main/java/com/example/michaeltraining/user/UserService.java
+++ src/main/java/com/example/michaeltraining/user/UserService.java
@@ -3,5 +3,5 @@ package com.example.michaeltraining.user;
 public interface UserService {
     UserDTO getUser(Long id);
     void saveUser(UserDTO dto);
-
+    void updateUser(Long id, User user);
 }
